1a876af8c60c13b3e79b215bb466389dd8bceb9b
Erutish <134590456+Erutish@users.noreply.github.com>
2024-04-20 22:31:27 +0600

    Add files via upload

---
+++ js/seller_script.js
@@ -0,0 +1,11 @@
+const userBtn=document.querySelector("#user-btn");
+userBtn.addEventListener( "click", ()=>{
+    const userBox=document.querySelector(".profile-detail");
+    userBox.classList.toggle('active');
+})
+
+const toggle=document.querySelector('.toggle-btn');
+toggle.addEventListener( 'click',() => {
+    const sidebar=document.querySelector('.sidebar');
+    sidebar.classList.toggle("active");
+});
